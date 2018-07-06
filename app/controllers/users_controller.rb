d57ae111947d44a81a01cca0530ca66ff206ae54
tranchien2002 <chie.tm2002@gmail.com>
2018-07-06 20:00:15 +0700

    sc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,24 +5,26 @@ class UsersController < ApplicationController
   def new
      @user = User.new
   end
-  def show
-    @user = User.find(params[:id])
-  end
 
   def create
      @user = User.new(user_params)   # Not the final implementation!
     if @user.save
       # Handle a successful save.
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+        flash[:info] = "Please check your email to activate your account."
+        redirect_to root_url
     else
       render 'new'
     end
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    redirect_to root_url and return unless FILL_IN
   end
 
   def update
